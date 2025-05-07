a27c68f4541640408aefec6ffe79c68eb6b53b96
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-07 15:01:23 -0300

    Commit inicial do projeto.

---
+++ src/main/java/com/mycompany/projeto/gamematch/ProjetoGameMatch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto.gamematch;
+
+/**
+ *
+ * @author alexandre-matiello
+ */
+public class ProjetoGameMatch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
